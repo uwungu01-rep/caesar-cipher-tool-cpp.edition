910ade7f80a2081ddcaee0dba4cfa943b40c0e63
UwuNgu01 <48872345+uwungu01-rep@users.noreply.github.com>
2024-11-02 09:46:33 +0700

    clean up the code a bit, slightly increase readability and less nesting

--- new.cpp
+++ new.cpp
@@ -2,151 +2,145 @@
 #include <string>
 #include <algorithm>
 
-using str = std::string;
-using std::cin;
-using std::cout;
+using str = std::string; 
+using std::cin; 
+using std::cout; 
+using std::getline;
 
-const str alp = "abcdefghijklmnopqrstuvwxyz";
-const str alpcap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const str alphabet = "abcdefghijklmnopqrstuvwxyz", upper_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-str chr;
-str shf;
-
-bool isDigit(str input) {
-    str temp = input;
-    if(temp.find("-") == 0) {
-        temp.str::erase(0);
+bool IsInt(str input) {
+    try {
+        stoi(input);
+        return true;
     }
-    return all_of(temp.begin(), temp.end(), ::isdigit);
+    catch(...) {
+        return false;
+    }
 }
 
-str sort(int shf, str alp) {
-    str cip = "";
-    int len = alp.length();
-    for(int i = shf; i < len + shf; i++) {
+str Trim(std::string& input) {
+    input.erase(0, input.find_first_not_of(" \t\n\r\f\v"));
+    input.erase(input.find_last_not_of(" \t\n\r\f\v") + 1);
+    return input;
+}
+
+str Sort(int shift, str alphabet) {
+    str ciphered = "";
+    int len = alphabet.length();
+    for(int i = shift; i < len + shift; i++) {
         if(i < len) {
-            cip += alp[i];
+            ciphered += alphabet[i];
             continue;
         }
-        cip += alp[abs(i - len)];
+        ciphered += alphabet[abs(i - len)];
     }
-    return cip;
+    return ciphered;
 }
 
-str sortcap(int shf, str alpcap) {
-    str cipcap = "";
-    int len = alpcap.length();
-    for(int i = shf; i < len + shf; i++) {
+str Sortcap(int shift, str upper_alphabet) {
+    str ciphered_upper = "";
+    int len = upper_alphabet.length();
+    for(int i = shift; i < len + shift; i++) {
         if(i < len) {
-            cipcap += alpcap[i];
+            ciphered_upper += upper_alphabet[i];
             continue;
         }
-        cipcap += alpcap[abs(i - len)];
+        ciphered_upper += upper_alphabet[abs(i - len)];
     }
-    return cipcap;
+    return ciphered_upper;
 }
 
-str Encipher(str alp, str cip, str alpcap, str cipcap, str chr) {
+str Encipher(str alphabet, str ciphered, str upper_alphabet, str ciphered_upper, str user_input) {
     str output = "";
-    for(int k = 0; k < chr.length(); k++) {
-        if(alp.find(chr[k]) != str::npos) {
-            output += cip[alp.find(chr[k])];
+    for(int k = 0; k < user_input.length(); k++) {
+        if(alphabet.find(user_input[k]) != str::npos) {
+            output += ciphered[alphabet.find(user_input[k])];
             continue;
         }
-        else if(alpcap.find(chr[k]) != str::npos) {
-            output += cipcap[alpcap.find(chr[k])];
+        else if(upper_alphabet.find(user_input[k]) != str::npos) {
+            output += ciphered_upper[upper_alphabet.find(user_input[k])];
             continue;
         }
-        output += chr[k];
+        output += user_input[k];
     }
     return output;
 }
 
-str Decipher(str alp, str cip, str alpcap, str cipcap, str chr) {
+str Decipher(str alphabet, str ciphered, str upper_alphabet, str ciphered_upper, str user_input) {
     str output = "";
-    for(int k = 0; k < chr.length(); k++) {
-        if(cip.find(chr[k]) != str::npos) {
-            output += alp[cip.find(chr[k])];
+    for(int k = 0; k < user_input.length(); k++) {
+        if(ciphered.find(user_input[k]) != str::npos) {
+            output += alphabet[ciphered.find(user_input[k])];
             continue;
         }
-        else if(cipcap.find(chr[k]) != str::npos) {
-            output += alpcap[cipcap.find(chr[k])];
+        else if(ciphered_upper.find(user_input[k]) != str::npos) {
+            output += upper_alphabet[ciphered_upper.find(user_input[k])];
             continue;
         }
-        output += chr[k];
+        output += user_input[k];
     }
     return output;
 }
 
-int algorithm(int shf) {
-    int temp = shf;
+int Algorithm(int shift) {
+    int temp = shift;
     if(temp >= 0) {
         temp = temp % 26;
         return temp;
     }
-    else {
-        while(temp < 0) {
-            temp = (26 + temp) % 26;
-        }
-        return temp;
+    while(temp < 0) {
+        temp = (26 + temp) % 26;
     }
+    return temp;
 }
 
 int main() {
-    str cmd = "";
+    str cmd = "", user_input, shift;
     while(true) {
-        cout << "Type E for deciphering, type D for deciphering (Case insensitive). Type / to exit the program: ";
-        std::getline(cin >> std::ws, cmd);
-        bool bck = true;
+        cout << "Type E for Deciphering, type D for Deciphering (Case insensitive). Type / to exit the program: "; getline(cin, cmd);
+        bool run = true;
         if(cmd == "e" || cmd == "E") {
-            while(true) {
-                if(bck) { //Start
-                    cout << "Your input: ";
-                    std::getline(cin >> std::ws, chr);
-                    while(true) {
-                        cout << "Shift (type / to cancel): ";
-                        std::getline(cin >> std::ws, shf);
-                        if(shf == "/") {
-                            bck = false;
-                            break;
-                        }
-                        else if(isDigit(shf)) {
-                            int temp = stoi(shf);
-                            cout << "Output: " << Encipher(alp, sort(algorithm(temp)), alp), alpcap, sortcap(algorithm(temp), alpcap), chr) << '\n';
-                        }
-                        else {
-                            cout << "Invalid input. \n";
-                        }
-                    }
+            while(run) {
+                cout << "Your input: "; getline(cin , user_input);
+                if(Trim(user_input).empty()) {
+                    cout << "Input cannot be empty. \n"; 
                     continue;
-                } //End
-                break;
+                }
+                while(run) {
+                    cout << "Shift (type / to cancel): "; getline(cin, shift);
+                    if(shift == "/") {
+                        run = false;
+                    }
+                    else if(IsInt(shift)) {
+                        cout << "Output: " << Encipher(alphabet, Sort(Algorithm(stoi(shift)), alphabet), upper_alphabet, Sortcap(Algorithm(stoi(shift)), upper_alphabet), user_input) << '\n';
+                    }
+                    else {
+                        cout << "Invalid input. \n";
+                    }
+                }
             }
         }
         else if(cmd == "d" || cmd == "D") {
-            while(true) {
-                if(bck) { //Start
-                    cout << "Your input: ";
-                    std::getline(cin >> std::ws, chr);
-                    while(true) {   
-                        cout << "Shift (type / to cancel): ";
-                        std::getline(cin >> std::ws, shf);
-                        if(shf == "/") {
-                            bck = false;
-                            break;
-                        }
-                        else if(isDigit(shf)) {
-                            int temp = stoi(shf);
-                            cout << "Output: " << Decipher(alp, sort(algorithm(temp)), alp), alpcap, sortcap(algorithm(temp), alpcap), chr) << '\n';
-                        }
-                        else {
-                            cout << "Invalid input. \n";
-                        }
-                    }
+            while(run) {
+                cout << "Your input: "; getline(cin, user_input);
+                if(Trim(user_input).empty()) {
+                    cout << "Input cannot be empty. \n";\
                     continue;
-                } //End
-                break;
-            }  
+                }
+                while(run) {
+                    cout << "Shift (type / to cancel): "; getline(cin, shift);
+                    if(shift == "/") {
+                        run = false;
+                    }
+                    else if(IsInt(shift)) {
+                        cout << "Output: " << Decipher(alphabet, Sort(Algorithm(stoi(shift)), alphabet), upper_alphabet, Sortcap(Algorithm(stoi(shift)), upper_alphabet), user_input) << '\n';
+                    }
+                    else {
+                        cout << "Invalid input. \n";
+                    }
+                }
+            }
         }
         else if(cmd == "/") {
             break;
@@ -156,4 +150,4 @@ int main() {
         }
     }
     return 0;
-}
+}    

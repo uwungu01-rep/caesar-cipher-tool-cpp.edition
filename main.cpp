98e20f78ffd77435a6ae833fc5dc7a9aaba5bb2a
UwuNgu01 <48872345+uwungu01-rep@users.noreply.github.com>
2024-12-09 21:55:39 +0700

    now with support for even larger shift because i can

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ using str = std::string;
 using std::cin;
 using std::cout;
 using std::getline;
+using std::stoll;
 
 str Trim(str input) {
     input.erase(0, input.find_first_not_of(" \t\n\r\f\v"));
@@ -50,16 +51,16 @@ str Sort(int shift, str alphabet) {
 
 str Encipher(str alphabet, str shifted, str upper_alphabet, str shifted_upper, str user_input) {
     str output = "";
-    for(int k = 0; k < user_input.length(); k++) {
-        if(alphabet.find(user_input[k]) != str::npos) {
-            output += shifted[alphabet.find(user_input[k])];
+    for(char k : user_input) {
+        if(alphabet.find(k) != str::npos) {
+            output += shifted[alphabet.find(k)];
             continue;
         }
-        else if(upper_alphabet.find(user_input[k]) != str::npos) {
-            output += shifted_upper[upper_alphabet.find(user_input[k])];
+        else if(upper_alphabet.find(k) != str::npos) {
+            output += shifted_upper[upper_alphabet.find(k)];
             continue;
         }
-        output += user_input[k];
+        output += k;
     }
     return output;
 }
@@ -80,21 +81,16 @@ str Decipher(str alphabet, str shifted, str upper_alphabet, str shifted_upper, s
     return output;
 }
 
-int Algorithm(int shift) {
-    int temp = shift;
-    if(temp >= 0) {
-        temp = temp % 26;
-        return temp;
+long long Algorithm(long long shift) {
+    if(shift >= 0) {
+        return shift % 26;
     }
-    while(temp < 0) {
-        temp = (26 + temp) % 26;
-    }
-    return temp;
+    return shift = 26 + shift % 26;
 }
 
 int main() {
     const str alphabet = "abcdefghijklmnopqrstuvwxyz", upper_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    str cmd = "", user_input, shift;
+    str cmd, user_input, shift;
     while(true) {
         cout << "Type E for Deciphering, type D for Deciphering (Case insensitive). Type / to exit the program: "; getline(cin, cmd);
         bool run = true;
@@ -104,7 +100,7 @@ int main() {
                 if(Trim(user_input).empty()) {
                     cout << "Input cannot be empty. \n"; 
                     continue;
-                }
+                }   
                 while(run) {
                     cout << "Shift (type / to cancel): "; getline(cin, shift);
                     if(shift == "/") {
@@ -112,16 +108,12 @@ int main() {
                     }
                     else if(IsInt(shift)) {
                         try {
-                            str shifted = Sort(Algorithm(stoi(shift)), alphabet);
-                            str shifted_upper = Sort(Algorithm(stoi(shift)), upper_alphabet);
+                            str shifted = Sort(Algorithm(stoll(shift)), alphabet);
+                            str shifted_upper = Sort(Algorithm(stoll(shift)), upper_alphabet);
                             cout << "Output: " << Encipher(alphabet, shifted, upper_alphabet, shifted_upper, user_input) << '\n';
                         }
                         catch(std::out_of_range) {
-                            if(shift[0] == '-')
-                                cout << "Shift is too small (min -2,147,483,648, max 2,147,483,647)! \n";
-                            else {
-                                cout << "Shift is too big (min -2,147,483,648, max 2,147,483,647)! \n";
-                            }
+                            cout << "Why? \n";
                         }
                     }
                     else {
@@ -144,16 +136,12 @@ int main() {
                     }
                     else if(IsInt(shift)) {
                         try {
-                            str shifted = Sort(Algorithm(stoi(shift)), alphabet);
-                            str shifted_upper = Sort(Algorithm(stoi(shift)), upper_alphabet);
+                            str shifted = Sort(Algorithm(stoll(shift)), alphabet);
+                            str shifted_upper = Sort(Algorithm(stoll(shift)), upper_alphabet);
                             cout << "Output: " << Decipher(alphabet, shifted, upper_alphabet, shifted_upper, user_input) << '\n';
                         }
                         catch(std::out_of_range) {
-                            if(shift[0] == '-')
-                                cout << "Shift is too small (min -2,147,483,648, max 2,147,483,647)! \n";
-                            else {
-                                cout << "Shift is too big (min -2,147,483,648, max 2,147,483,647)! \n";
-                            }
+                            cout << "Why? \n";
                         }
                     }
                     else {

1395445f6b7146510aa5055d25beee4a872b5792
UwuNgu01 <48872345+uwungu01-rep@users.noreply.github.com>
2025-01-14 18:34:19 +0700

    update

--- Caesar.cpp
+++ Caesar.cpp
@@ -2,32 +2,35 @@
 #include <cstdlib>
 #include "Caesar.h"
 
-int Caesar::Algorithm(long long shift) {
+using str = std::string;
+
+short Caesar::Algorithm(long long shift) {
     if(shift >= 0) {
         return shift % 26;
     }
     return 26 + shift % 26;
 }
 
-std::string Caesar::Sort(int shift, std::string alphabet) {
-    std::string shifted = "";
+str Caesar::Sort(int shift, str alphabet) {
+    str shifted = "";
     int len = alphabet.length();
     for(int i = shift; i < len + shift; i++) {
         if(i < len) {
             shifted += alphabet[i];
-            continue;
         }
-        shifted += alphabet[std::abs(i - len)];
+        else {
+            shifted += alphabet[std::abs(i - len)];
+        }
     }
     return shifted;
 }
-std::string Caesar::Cipher(std::string alphabet, std::string shifted, std::string upper_alphabet, std::string shifted_upper, std::string user_input) {
-    std::string output = "";
+str Caesar::Cipher(str alphabet, str shifted, str upper_alphabet, str shifted_upper, str user_input) {
+    str output = "";
     for(char k : user_input) {
-        if(alphabet.find(k) != std::string::npos) {
+        if(alphabet.find(k) != str::npos) {
             output += shifted[alphabet.find(k)];
         }
-        else if(upper_alphabet.find(k) != std::string::npos) {
+        else if(upper_alphabet.find(k) != str::npos) {
             output += shifted_upper[upper_alphabet.find(k)];
         }
         else {
--- Caesar.h
+++ Caesar.h
@@ -7,7 +7,7 @@ class Caesar {
     public:
         static std::string Cipher(std::string alphabet, std::string shifted, std::string upper_alphabet, std::string shifted_upper, std::string user_input);
         static std::string Sort(int shift, std::string alphabet);
-        static int Algorithm(long long shift);
+        static short Algorithm(long long shift);
 };
 
 #endif
